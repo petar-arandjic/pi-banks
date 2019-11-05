055a694a65651dbf6377c98ab9c9e940519555d3
Petar <parandjic@gmail.com>
2019-11-05 01:12:02 +0100

    added statistics

--- resources/js/components/AppBankeComponents/Bank.MainInfo.vue
+++ resources/js/components/AppBankeComponents/Bank.MainInfo.vue
@@ -1,28 +1,71 @@
 <template>
     <div class="global">
+        <b-container fluid class="p-0">
+            <b-row>
+            <b-card
+                bg-variant="light"
+                header="Statistika svih računa"
+                class="text-center mt-2 mr-4 ml-2 w-100"
+                no-body
+            >
+                <b-row class="pl-2 pr-2 pt-2 pb-2">
+                    <b-col class="text-left">Ukupni prihodi:</b-col>
+                    <b-col class="text-right">{{ Stats.income }}</b-col>
+                </b-row>
+                <b-row class="pl-2 pr-2 pt-2 pb-2">
+                    <b-col class="text-left">Ukupni troškovi:</b-col>
+                    <b-col class="text-right">{{ Stats.expenses }}</b-col>
+                </b-row>
+                <b-row class="pl-2 pr-2 pt-2 pb-2">
+                    <b-col class="text-left">Početno stanje:</b-col>
+                    <b-col class="text-right">{{ Stats.starting_balance }}</b-col>
+                </b-row>
+                <b-row class="pl-2 pr-2 pt-2 pb-2">
+                    <b-col class="text-left">Dozvoljeni minus:</b-col>
+                    <b-col class="text-right">{{ Stats.allowed_overdraft }}</b-col>
+                </b-row>
+                <b-row class="pl-2 pr-2 pt-2 pb-2">
+                    <b-col class="text-left">Profit:</b-col>
+                    <b-col class="text-right">{{ Stats.profit }}</b-col>
+                </b-row>
+                <b-row class="pl-2 pr-2 pt-2 pb-2">
+                    <b-col class="text-left">Stanje računa:</b-col>
+                    <b-col class="text-right">{{ Stats.balance }}</b-col>
+                </b-row>
+            </b-card>
+        </b-row>
+        </b-container>
         <div class="db-newTransaction">
-            <button class="b-newTransaction">DODAJ NOVU TRANSAKCIJU</button>
+            <div class="row">
+                <button type="button" class="btn btn-danger w-100 m-2 mr-4"><strong><a class="bank_link" href="transaction/create">Dodaj novu transakciju</a></strong></button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    props:{
+        Stats: Object,
+    },
+    methods:{
+        test(){
+            //console.log(this.Stats);
+        }
+    }
 }
 </script>
 
 
 <style lang="scss">
     .global{
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
     }
     .db-newTransaction{
         position: absolute;
-        bottom: 5px;
-        left: 5px;
-        right: 5px;
-        height: 30px;
+        bottom: 0px;
+        left: 0px;
+        right: 0px;
     }
     .b-newTransaction{
         position: absolute;
@@ -31,11 +74,17 @@ export default {
         background: #E53935;
         color: white;
         font-weight: bold;
+        font-size: 1vw;
     }
     .b-newTransaction:hover{
         opacity: 0.9;
     }
     .b-newTransaction:active{
         opacity: 0.8;
+    }
+    .mi-balance{
+        position: relative;
+        text-align: center;
+        margin: 10px;
     }
 </style>

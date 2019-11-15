7ea0f34f52181a4a9f0aa94ec8f5e1f16c968b84
Petar <parandjic@gmail.com>
2019-11-15 14:53:00 +0100

    working on accounts table

--- resources/js/components/AppBanksComponents/BankInfo.vue
+++ resources/js/components/AppBanksComponents/BankInfo.vue
@@ -21,11 +21,11 @@
                 <table class="table table-bordered table-hover text-center border-bottom">
                     <thead class="thead-dark">
                         <tr>
-                            <th class="sticky-header p-1" width="20%" >račun</th>
-                            <th class="sticky-header p-1" width="20%" >početno stanje</th>
+                            <th class="sticky-header p-1" width="25%" >račun</th>
+                            <th class="sticky-header p-1" width="25%" >početno stanje</th>
                             <th class="sticky-header p-1" width="20%" >dozvoljeni minus</th>
                             <th class="sticky-header p-1" width="20%" >stanje</th>
-                            <th class="sticky-header p-1" width="20%" >valuta</th>
+                            <th class="sticky-header p-1" width="10%" >valuta</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -54,30 +54,41 @@
                 </table>
             </b-row>
             <b-row class="m-0">
+                <form id="account_form" class="w-100">
                 <table class="table table-bordered table-hover text-center border-bottom mb-0">
-                    <thead class="border">
-                        <tr>
-                            <th class="sticky-header p-1" width="20%">
-                                <input type="number" class="border-0 w-100 h-100 text-center" placeholder="račun">
-                            </th>
-                            <th class="sticky-header p-1" width="20%">
-                                <input type="number" class="border-0 w-100 h-100 text-center" placeholder="početno stanje">
-                            </th>
-                            <th class="sticky-header p-1" width="20%">
-                                dozvoljeni minus</th>
-                            <th class="sticky-header p-1" width="20%">stanje
-
-                            </th>
-                            <th class="sticky-header p-1" width="20%">
-                                valuta
-                            </th>
-                        </tr>
-                    </thead>
+                        <thead class="border">
+                            <tr>
+                                <th class="sticky-header p-1" width="25%">
+                                    <input name="account" type="number" class="border-0 w-100 h-100 text-center" placeholder="račun">
+                                </th>
+                                <th class="sticky-header p-1" width="25%">
+                                    <input name="starting_balance" type="number" class="border-0 w-100 h-100 text-center" placeholder="početno stanje">
+                                </th>
+                                <th class="sticky-header p-1" width="20%">
+                                    <input name="allowed_overdraft" type="number" class="border-0 w-100 h-100 text-center" placeholder="dozvoljeni minus">
+                                </th>
+                                <th class="sticky-header p-1" width="20%">
+                                    
+                                </th>
+                                <th class="sticky-header p-1" width="10%">
+                                    <select name="currency" class="w-100 h-100 border-0">
+                                        <option value="KM">KM</option>
+                                        <option value="EUR">EUR</option>
+                                        <option value="USD">USD</option>
+                                    </select>
+                                </th>
+                            </tr>
+                        </thead>
                 </table>
+                </form>
             </b-row>
             <b-row class="m-0 mt-1">
                 <b-col md="1" offset="11" class="p-0">
-                    <button type="button" class="btn btn-dark w-100">Sačuvaj</button>
+                    <button type="button" class="btn btn-dark w-100"
+                        @click="submit_account"
+                    >
+                    Sačuvaj
+                    </button>
                 </b-col>
             </b-row>
         </div>
@@ -88,6 +99,25 @@
 export default {
     props: {
         bank: Object
+    },
+    methods: {
+        submit_account(){
+            const myForm = document.getElementById('account_form');
+            const formData = new FormData(myForm);
+
+            const url = "api/bank/" + this.bank.id + "/account/store";
+
+            fetch(url, {
+                method: 'post',
+                body: formData
+            }).then((response)=>{
+                return response.text();
+            }).then((response)=>{
+                console.log(response);
+            }).catch((error)=>{
+                console.error(error);
+            });
+        }
     }
 }
 </script>

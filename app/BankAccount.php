0148dac0fcc3a8fd83e9185225d08ffd2d433301
Petar <parandjic@gmail.com>
2019-11-18 22:47:24 +0100

    added filleble

--- app/BankAccount.php
+++ app/BankAccount.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class BankAccount extends Model
 {
     //
+    protected $fillable = ['account', 'starting_balance', 'allowed_overdraft', 'currency_id', 'bank_id'];
+
     public function bank(){
         return $this->belongsTo('Bank');
     }

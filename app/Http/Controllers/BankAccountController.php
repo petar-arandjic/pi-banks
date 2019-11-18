4915525dcb6f95c65289332ea9975bb829c60b17
Petar <parandjic@gmail.com>
2019-11-18 17:23:52 +0100

    Store methode now working testing needed

--- app/Http/Controllers/BankAccountController.php
+++ app/Http/Controllers/BankAccountController.php
@@ -4,13 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+
+use App\BankAccount;
+
+use App\Bank;
+
+use Illuminate\Support\Facades\Validator;
+
 class BankAccountController extends Controller
 {
     //
-    public function store($id){
-        //print_r(request()->all());
-        $data = request()->validate([
-            'account' => ['requred', 'numeric']
-        ]);
+    public function store(Request $request, $id){
+
+        # Check if Bank exist
+        if(Bank::find($id)){
+            # Validate data
+            $data = Validator::make($request->all(), [
+                'account'               =>      ['required','digits:13'],
+                'starting_balance'      =>      ['required', 'numeric', 'Between: 0, 50000000'],
+                'allowed_overdraft'     =>      ['required', 'numeric', 'Between: 0, 50000000'],
+                'currency'              =>      ['required', 'exists:currencies'],
+            ]);
+
+            if ($data->fails()) {
+                $response = [
+                    'errors' => $data->errors()
+                ];
+                return $response;
+            }else{
+                $data->create();
+            }
+        }
     }
 }

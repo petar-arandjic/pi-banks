b7f47c4c920f2f5cfaad08fab1f4e342bda614f0
Petar <parandjic@gmail.com>
2019-11-18 18:59:37 +0100

    removed account from table

--- app/Http/Resources/Bank.php
+++ app/Http/Resources/Bank.php
@@ -19,7 +19,6 @@ class Bank extends JsonResource
         return [
             'id'                =>      $this->id,
             'name'              =>      $this->name,
-            'account'           =>      $this->account,
             'allowed_overdraft' =>      $this->allowed_overdraft,
             'bg_color'          =>      $this->bg_color,
             'font_color'        =>      $this->font_color,

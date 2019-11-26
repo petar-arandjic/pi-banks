205bf6df1fa1727ed4cc7d664300a8ac4dd17030
Petar <parandjic@gmail.com>
2019-11-26 17:16:10 +0100

    changes to relationships

--- app/Bank.php
+++ app/Bank.php
@@ -4,24 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-use App\BankStyle;
-
-use App\Transaction;
-
 
 class Bank extends Model{
 
     protected $fillable = ['name', 'account', 'style_id', 'allowed_overdraft', 'starting_balance'];
 
-    //style for fron end bank cards
-    public function bankStyle(){
-        return $this->hasOne('BankStyle');
-    }
-    //Transactions for every bank
-    public function transaction(){
-        return $this->hasMany('Transaction');
-    }
-    public function bankAccount(){
-        return $this->hasMany('BankAccaount');
+    public function bankAccounts(){
+        return $this->hasMany('App\BankAccount', 'bank_id');
     }
 }

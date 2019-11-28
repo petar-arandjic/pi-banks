18dc2b41335f39652274ff267ecfb8f1239cdc5d
Petar <parandjic@gmail.com>
2019-11-28 18:34:57 +0100

    added font awesome

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
+    <link rel="stylesheet" href="{{asset('icons/fontawesome/css/all.css')}}" />
     <title>Document</title>
     <style>
         body{
